7a7473027bbddbc10ba878b7668ea89a12ba509f
유영재 <57673815+pdlma0904@users.noreply.github.com>
2021-05-16 19:20:21 +0900

    Update GetMovie.ts

--- src/Api/GetMovie.ts
+++ src/Api/GetMovie.ts
@@ -1,10 +1,24 @@
 import axios from "axios";
 
 const Movie_URL =
-  "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=91241eddbc52fd0dcb782e71b74a0f52";
+  "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888";
 
 export const GetMovieData = (date: object) => {
-  const dateReceived = Object.values(date).join("");
-  const request = axios.get(Movie_URL + "&targetDt=" + dateReceived);
-  console.log(`request`, request);
+  const dateReceived = Object.values(date);
+  for (let i = 0; i < dateReceived.length; i++) {
+    if (dateReceived[i] < 10) {
+      dateReceived[i] = "0" + dateReceived[i];
+    } else {
+      dateReceived[i] = dateReceived[i];
+    }
+  }
+  const newDate = dateReceived.join("");
+  const request = async () => {
+    const data = await axios
+      .get(Movie_URL + "&targetDt=" + newDate)
+      .then((res) => res.data.boxOfficeResult.dailyBoxOfficeList);
+    return data;
+  };
+
+  console.log(`request`, request());
 };
